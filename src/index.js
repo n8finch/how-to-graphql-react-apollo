2e2ebcac4a30c186b2aa1eb22fbf97420cd3e2c5
Nate Finch <finchproservices@gmail.com>
2019-06-19 07:39:01 -0500

    links loaded🤓

--- src/index.js
+++ src/index.js
@@ -22,9 +22,11 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 })
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// 4
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 serviceWorker.unregister();
